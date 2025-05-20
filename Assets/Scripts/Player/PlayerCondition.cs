5074c57c166ffeb905dc1c49920d4d876f5b291d
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-05-20 19:22:00 +0900

    feat: Health UI

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damage);
+}
+
+public class PlayerCondition : MonoBehaviour //, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition health {get {return uiCondition.health;}}
+    // Condition hunger {get {return uiCondition.hunger;}}
+    // Condition stamina {get {return uiCondition.stamina;}}
+
+    // public float noHungerHealthDecay;
+    
+    // // Delegate
+    // public event Action onTakeDamage;
+
+    void Update()
+    {
+        // hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        // stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        // if (hunger.curValue == 0f) 
+        // {
+        //     health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        // } 
+
+        if (health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amout) 
+    {
+        health.Add(amout);
+    }
+
+    // public void Eat(float amout)
+    // {
+    //     hunger.Add(amout);
+    // }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+    }
+
+    // public void TakePhysicalDamage(int damage)
+    // {
+    //     health.Subtract(damage);
+    //     onTakeDamage?.Invoke();
+    // }
+}
